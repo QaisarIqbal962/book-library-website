e6144fbe45ab89d57bf35ffdab0d00b11de66a8c
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-12-02 12:31:29 +0500

    clean project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <h1>test two</h1>
+      <h1>test</h1>
+      <h1></h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
